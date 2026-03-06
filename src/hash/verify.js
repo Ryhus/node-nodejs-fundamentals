b4957a5d8d6761e3bca4b8d44bcc7a29c80d11b5
Ryhus <ryhusyevhen@gmail.com>
2026-03-06 23:05:43 +0100

    feat: add solution for verify.js

--- src/hash/verify.js
+++ src/hash/verify.js
@@ -1,8 +1,61 @@
+import { createHash } from "node:crypto";
+import { readdir, readFile } from "node:fs/promises";
+import { createReadStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+import path from "node:path";
+import { error } from "node:console";
+
 const verify = async () => {
   // Write your code here
   // Read checksums.json
   // Calculate SHA256 hash using Streams API
   // Print result: filename — OK/FAIL
+
+  const pathTofilesDirectory = path.resolve(
+    process.cwd(),
+    "src",
+    "hash",
+    "files",
+  );
+  const pathToChecksums = path.resolve(
+    process.cwd(),
+    "src",
+    "hash",
+    "checksums.json",
+  );
+
+  async function hashFile(file, pathTofilesDirectory) {
+    const hash = createHash("sha256");
+    const pathToFile = path.join(pathTofilesDirectory, file);
+
+    await pipeline(createReadStream(pathToFile), hash);
+
+    return [file, hash.digest("hex")];
+  }
+
+  const [checksumsText, files] = await Promise.all([
+    readFile(pathToChecksums, {
+      encoding: "utf-8",
+    }),
+    readdir(pathTofilesDirectory),
+  ]).catch(() => {
+    throw new Error("FS operation failed");
+  });
+
+  const checksumsObject = JSON.parse(checksumsText);
+
+  const hashes = await Promise.all(
+    files.map((file) => hashFile(file, pathTofilesDirectory)),
+  );
+
+  hashes.forEach((el) => {
+    const key = el[0];
+    if (checksumsObject[key] === el[1]) {
+      console.log(`${key} - OK`);
+    } else {
+      console.log(`${key} - FALSE`);
+    }
+  });
 };
 
 await verify();
