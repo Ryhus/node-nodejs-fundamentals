53b83bd6016c2ad703b07e4921a8f25da5b63190
Ryhus <ryhusyevhen@gmail.com>
2026-03-07 15:47:25 +0100

    feat: add solution for worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,9 +1,18 @@
-import { parentPort } from 'worker_threads';
+import { parentPort, isMainThread, Worker } from "worker_threads";
 
 // Receive array from main thread
 // Sort in ascending order
 // Send back to main thread
 
-parentPort.on('message', (data) => {
-  // Write your code here
-});
+if (isMainThread) {
+  const worker = new Worker(new URL(import.meta.url));
+  worker.on("message", (msg) => console.log("Sorted Array: ", msg));
+
+  const arrayToSort = [7, 2, 3, 8, 10];
+  worker.postMessage(arrayToSort);
+} else {
+  parentPort.on("message", (data) => {
+    const sortedArray = data.sort((a, b) => a - b);
+    parentPort.postMessage(sortedArray);
+  });
+}
