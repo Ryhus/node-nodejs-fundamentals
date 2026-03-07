9b54ef704b0a0698cfc8d0b10e14f6313294936e
Ryhus <ryhusyevhen@gmail.com>
2026-03-07 13:11:58 +0100

    feat: add solution for compressDir.js

--- src/zip/compressDir.js
+++ src/zip/compressDir.js
@@ -1,9 +1,65 @@
+import { createBrotliCompress } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { readdir, access, stat, mkdir } from "node:fs/promises";
+import { pipeline } from "node:stream/promises";
+import path from "node:path";
+
 const compressDir = async () => {
   // Write your code here
   // Read all files from workspace/toCompress/
   // Compress entire directory structure into archive.br
   // Save to workspace/compressed/
   // Use Streams API
+
+  const pathToFiles = path.resolve(
+    process.cwd(),
+    "home",
+    "user",
+    "workspace",
+    "toCompress",
+  );
+  const targetDir = path.resolve(
+    process.cwd(),
+    "home",
+    "user",
+    "workspace",
+    "compressed",
+  );
+
+  try {
+    await access(pathToFiles);
+  } catch {
+    throw new Error("FS operation failed");
+  }
+
+  const archivePath = path.join(targetDir, "archive.br");
+
+  await mkdir(targetDir, { recursive: true });
+  const files = await readdir(pathToFiles, { recursive: true });
+  const fullPathes = files.map((file) => path.join(pathToFiles, file));
+
+  const brotli = createBrotliCompress();
+  const output = createWriteStream(archivePath);
+
+  brotli.pipe(output);
+
+  for (const file of fullPathes) {
+    const st = await stat(file);
+    const pathBuf = Buffer.from(file);
+    const pathLen = Buffer.alloc(4);
+    pathLen.writeUInt32BE(pathBuf.length);
+
+    const sizeBuf = Buffer.alloc(8);
+    sizeBuf.writeBigUInt64BE(BigInt(st.size));
+
+    brotli.write(pathLen);
+    brotli.write(pathBuf);
+    brotli.write(sizeBuf);
+
+    await pipeline(createReadStream(file), brotli, { end: false });
+  }
+
+  brotli.end();
 };
 
 await compressDir();
